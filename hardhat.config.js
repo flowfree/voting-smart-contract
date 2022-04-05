7295e0febc8c2e52f41f9b105aa57faba23d93c1
Nashruddin Amin <nashruddin.amin@gmail.com>
2022-04-05 08:53:14 +0700

    Updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,15 +3,16 @@ require('dotenv').config()
 
 const { ALCHEMY_API_KEY, RINKEBY_PRIVATE_KEY } = process.env;
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+const config = {
   solidity: "0.8.13",
-  networks: {
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${RINKEBY_PRIVATE_KEY}`]
-    }
+  networks: {}
+}
+
+if (ALCHEMY_API_KEY && RINKEBY_PRIVATE_KEY) {
+  config.networks.rinkeby = {
+    url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+    accounts: [`${RINKEBY_PRIVATE_KEY}`]
   }
-};
+}
+
+module.exports = config;
